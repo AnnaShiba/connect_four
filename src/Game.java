f966370ef2e18484125b02de4f8027c621f84e28
Anna Shiba <102438674+AnnaShiba@users.noreply.github.com>
2024-03-31 20:18:31 -0400

    Working minmax AI

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
+    private final int winningScore = 100;
     private final int boardColumns = 7;
     private final int boardRows = 6;
     private String[] board;
@@ -11,29 +12,34 @@ public class Game {
     private String symbol1;
     private String symbol2;
 
+    private boolean isAIGame;
+
 
     public Game(){
         board = new String[boardColumns * boardRows];
         Arrays.fill(board, "O");
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Hello gamer! Please, enter player 1 name: ");
-        playerName1 = input.nextLine();
+//        System.out.println("Hello gamer! Please, enter player 1 name: ");
+//        playerName1 = input.nextLine();
+//
+//        System.out.println(playerName1 + ", choose symbol R (for Red) or Y (for Yellow): ");
+//        boolean symbolEntered = false;
+//        while (!symbolEntered) {
+//            symbol1 = input.nextLine().toUpperCase();
+//
+//            if (!symbol1.equals("R") && !symbol1.equals("Y")) {
+//                System.out.println("Only R (for Red) or Y (for Yellow) allowed! Again: ");
+//            } else {
+//                symbolEntered = true;
+//            }
+//        }
+        playerName1 = "Bobert";
+        symbol1 = "R";
+        playerName2 = "YAI";
 
-        System.out.println(playerName1 + ", choose symbol R (for Red) or Y (for Yellow): ");
-        boolean symbolEntered = false;
-        while (!symbolEntered) {
-            symbol1 = input.nextLine().toUpperCase();
-
-            if (!symbol1.equals("R") && !symbol1.equals("Y")) {
-                System.out.println("Only R (for Red) or Y (for Yellow) allowed! Again: ");
-            } else {
-                symbolEntered = true;
-            }
-        }
-
-        System.out.println("Please, enter player 2 name: ");
-        playerName2 = input.nextLine();
+//        System.out.println("Please, enter player 2 name: ");
+//        playerName2 = input.nextLine();
         if (symbol1.equals("R")) {
             symbol2 = "Y";
         } else {
@@ -52,6 +58,10 @@ public class Game {
         }
     }
 
+    public boolean isEmpty(int position) {
+        return board[position].equals("O");
+    }
+
     public void playGame(){
         printBoard(board);
         while (true) {
@@ -59,10 +69,36 @@ public class Game {
                 System.out.println("Congratulations " + playerName1 + "! You win!");
                 break;
             }
-            if (playerAction(playerName2, symbol2)) {
-                System.out.println("Congratulations " + playerName2 + "! You win!");
+
+            int bestMove = -1;
+            int bestScore = -100;
+            for (int i = 0; i < boardColumns; i++ ) {
+                if (!isEmpty(i)) {
+                    continue;
+                }
+
+                int score = max(board, symbol2, i, 7);
+
+                if (score > bestScore) {
+                    bestMove = i;
+                    bestScore = score;
+                }
+            }
+
+            if (bestMove == -1) {
+                System.out.println("We ran out of valid moves. Goodbye.");
                 break;
             }
+            if (dropDisk(board, symbol2, bestMove)) {
+                printBoard(board);
+                if (checkWinner(board)) {
+                    System.out.println("AI wins today! Try again!");
+                }
+            }
+//            if (playerAction(playerName2, symbol2)) {
+//                System.out.println("Congratulations " + playerName2 + "! You win!");
+//                break;
+//            }
         }
     }
     public boolean playerAction(String playerName, String symbol) {
@@ -97,18 +133,18 @@ public class Game {
     }
 
     public boolean dropDisk(String[] board, String symbol, int position) {
-        boolean isEmpty = board[position].equals("O");
+        boolean isEmpty = isEmpty(position);
+        if (!isEmpty) {
+            return false;
+        }
+
         while (isEmpty && position + boardColumns < board.length) {
             int newPosition = position + boardColumns;
-            isEmpty = board[newPosition].equals("O");
+            isEmpty = isEmpty(newPosition);
 
             if (isEmpty) {
                 position = newPosition;
             }
-        }
-
-        if (!board[position].equals("O")) {
-            return false;
         }
 
         board[position] = symbol;
@@ -164,10 +200,10 @@ public class Game {
             return false;
         }
 
-        return checkLine(cell+change, change, symbol, 1);
+        return checkLine(board, cell+change, change, symbol, 1);
     }
 
-    public boolean checkLine(int cell, int change, String symbol, int count){
+    public boolean checkLine(String[] board, int cell, int change, String symbol, int count){
         if (!symbol.equals(board[cell])) {
             return false;
         }
@@ -176,58 +212,125 @@ public class Game {
         if (count == 4) {
             return true;
         }
-        return checkLine(cell+change, change, symbol, count);
+        return checkLine(board,cell+change, change, symbol, count);
     }
 
-    public int max(String[] board, String symbol, int column, int movesCount) {
+    public int max(String[] board, String symbol, int column, int depth) {
         String[] newBoard = board.clone();
+        boolean validMove = dropDisk(newBoard, symbol, column);
+        int score = scoreBoard(board, symbol);
 
-        if (dropDisk(newBoard, symbol, column)) {
-            // something went wrong here
-            System.out.println("Check logic, AI stooopid");
-            return -100;
-        }
-        if (checkWinner(newBoard)) {
-            return 50 - movesCount;
-        }
-
-        int maxValue = -100;
-        for (int i = 0; i < boardColumns; i++) {
-            if (!newBoard[i].equals("O")) {
-                continue;
+        if (validMove && depth > 0) {
+            int maxValue = -100;
+            for (int i = 0; i < boardColumns; i++) {
+                int otherScore = min(newBoard, symbol, i, depth - 1);
+                if (score > maxValue) {
+                    maxValue = otherScore;
+                }
             }
-
-            int score = min(newBoard, symbol, i, movesCount + 1);
-            if (score > maxValue) {
-                maxValue = score;
-            }
+            return maxValue;
         }
-        return maxValue;
+        return score;
     }
 
-    public int min(String[] board, String symbol, int column, int movesCount) {
+    public int min(String[] board, String symbol, int column, int depth) {
         String[] newBoard = board.clone();
-
-        if (dropDisk(newBoard, symbol, column)) {
-            // something went wrong here
-            System.out.println("Check logic, AI stooopid");
-            return -100;
-        }
-        if (checkWinner(newBoard)) {
-            return -50 - movesCount;
+        String oppositeSymbol;
+        if (symbol.equals("R")) {
+            oppositeSymbol = "Y";
+        } else {
+            oppositeSymbol = "R";
         }
 
-        int minValue = 100;
-        for (int i = 0; i < boardColumns; i++) {
-            if (!newBoard[i].equals("O")) {
-                continue;
+        boolean validMove = dropDisk(newBoard, oppositeSymbol, column);
+        int score = scoreBoard(board, symbol);
+
+        if (validMove && depth > 0) {
+            int maxValue = -100;
+            for (int i = 0; i < boardColumns; i++) {
+                int otherScore = max(newBoard, symbol, i, depth - 1);
+                if (score > maxValue) {
+                    maxValue = otherScore;
+                }
             }
+            return maxValue;
+        }
+        return score;
+    }
 
-            int score = max(newBoard, symbol, i, movesCount + 1);
-            if (score < minValue) {
-                minValue = score;
+    public int scoreBoard(String[] board, String playerSymbol){
+        int totalScore = 0;
+        // horizontal
+        for (int row = 0; row < boardRows; row++) {
+            for (int column = 0; column < boardColumns - 3; column++) {
+                int cell = row * boardColumns + column;
+                int score = scoreLine(board, cell, 1);
+                if (!board[cell].equals(playerSymbol)) {
+                    score *= -1;
+                }
+                totalScore += score;
             }
         }
-        return minValue;
+
+        // vertical
+        for (int row = 0; row < boardRows - 3; row++) {
+            for (int column = 0; column < boardColumns; column++) {
+                int cell = row * boardColumns + column;
+                int score = scoreLine(board, cell, boardColumns);
+                if (!board[cell].equals(playerSymbol)) {
+                    score *= -1;
+                }
+                totalScore += score;
+            }
+        }
+
+        // diagonal ascending
+        for (int row = 0; row < boardRows - 3; row++) {
+            for (int column = 3; column < boardColumns; column++) {
+                int cell = row * boardColumns + column;
+                int score = scoreLine(board, cell, boardColumns - 1);
+                if (!board[cell].equals(playerSymbol)) {
+                    score *= -1;
+                }
+                totalScore += score;
+            }
+        }
+
+        // diagonal descending
+        for (int row = 0; row < boardRows - 3; row++) {
+            for (int column = 0; column < boardColumns - 3; column++) {
+                int cell = row * boardColumns + column;
+                int score = scoreLine(board, cell, boardColumns + 1);
+                if (!board[cell].equals(playerSymbol)) {
+                    score *= -1;
+                }
+                totalScore += score;
+            }
+        }
+        return totalScore;
+    }
+
+    public int scoreLine(String[] board, int cell, int change){
+        String symbol = board[cell];
+        if (symbol.equals("O")) {
+            return 0;
+        }
+
+        return scoreLine(board, cell+change, change, symbol, 1, 1);
+    }
+
+    public int scoreLine(String[] board, int cell, int change, String symbol, int count, int score) {
+        if (isEmpty(cell)) {
+            return score;
+        }
+        if (!symbol.equals(board[cell])) {
+            return 0;
+        }
+
+        count++;
+        if (count == 4) {
+            return winningScore;
+        }
+        return scoreLine(board,cell+change, change, symbol, count, score * 2);
     }
 }
